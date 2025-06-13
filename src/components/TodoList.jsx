6433c3b57f753b2bc75b6cac257344fdafe5d1a0
amrishs590 <amrishs590@gmail.com>
2025-06-13 12:15:04 +0530

    Updated Edit task

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -9,7 +9,7 @@ const TodoList = () => {
   }
   function addTask() {
     if (task === "") return;
-    setTasks([...tasks, { text: task, completed: false }]);
+    setTasks([...tasks, { text: task, completed: false, edit: false }]);
     setTask("");
   }
   function DeleteTask(index) {
@@ -24,10 +24,25 @@ const TodoList = () => {
         return { ...item, completed: !item.completed };
       }
       return item;
-    }); 
+    });
     setTasks(updatedTasks);
   }
 
+  function EditTask(index) {
+    const newtask = tasks.map((item, i) => {
+      if (i == index) return { ...item, edit: true };
+      return item;
+    });
+    setTasks(newtask);
+  }
+
+  function SaveTask(index, nt) {
+    const newtask = tasks.map((item, i) => {
+      if (i == index) return { ...item, text: nt, edit: false };
+      return item;
+    });
+    setTasks(newtask);
+  }
   return (
     <>
       <div className="todo-wrapper">
@@ -44,11 +59,38 @@ const TodoList = () => {
           {tasks.map((item, index) => {
             return (
               <li key={index}>
-                {item.text}
-                <button className="statusbtn" onClick={() => TaskStatus(index)}>
-                  {item.completed ? "✅" : "❌"}
-                </button>
-                <button onClick={() => DeleteTask(index)}>Delete Task</button>
+                {item.edit ? (
+                  <>
+                    <input
+                      type="text"
+                      defaultValue={item.text}
+                      onChange={(e) => (item.text = e.target.value)}
+                    />
+                    <button onClick={() => SaveTask(index, item.text)}>
+                      Save
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    {item.text}
+                    <button
+                      className="statusbtn"
+                      onClick={() => TaskStatus(index)}
+                    >
+                      {item.completed ? "✅" : "❌"}
+                    </button>
+                    <button
+                      style={{ backgroundColor: "blue" }}
+                      onClick={() => EditTask(index)}
+                    >
+                      Edit
+                    </button>
+
+                    <button onClick={() => DeleteTask(index)}>
+                      Delete Task
+                    </button>
+                  </>
+                )}
               </li>
             );
           })}
